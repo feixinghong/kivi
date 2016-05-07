a7acec249337d0a766e9370784bc570d758f772a
Boris Kaul <localvoid@gmail.com>
2016-05-07 13:06:39 +0600

    Update 2016-05-07T07:06:34.787Z

--- examples/intro/main.js
+++
@@ -1,8 +0,0 @@
-const Main = new kivi.ComponentDescriptor()
-  .update((c) => {
-    c.sync(kivi.createVRoot().children(`Hello ${c.data}`));
-  });
-
-document.addEventListener('DOMContentLoaded', () => {
-  kivi.injectComponent(Main, document.body, 'world');
-});
--- tests/index.spec.ts
+++
@@ -1,5 +0,0 @@
-import "./vdom.spec";
-import "./scheduler.spec";
-import "./vmodel.spec.ts";
-import "./component.spec";
-import "./container_manager";
